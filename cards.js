3e389aed03e8180483ab401506250fa6c88cb1b1
Makieva <maeva.oukid@gmail.com>
2023-05-09 13:29:15 +0200

    retour version avant burger

--- cards.js
+++ cards.js
@@ -3,6 +3,6 @@ const cards = document.querySelectorAll(".block_images .card");
 cards.forEach((card, idx) => {
   const btn = card.querySelector("button");
   btn.addEventListener("click", () => {
-    window.location = `http://127.0.0.1:3000/details.html?pizzaId=${idx}`;
+    window.location = `./details.html?pizzaId=${idx}`;
   });
 });
